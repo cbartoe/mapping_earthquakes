c89257bc80f4b45622fdc7e8e8fd29c3ca545b15
Colin Bartoe <colinbartoe@Colins-MacBook-Pro.local>
2021-05-26 20:09:00 -0400

    updating for the geoJSON url pull

--- mapping_geojson_points/static/js/logic.js
+++ mapping_geojson_points/static/js/logic.js
@@ -2,38 +2,49 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [30.1955, -97.6662],
-    [43.6773, -79.6247],
-    [40.6448, -73.7846]
-  ];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "darkblue",
-    dashArray: 10,
-    weight: 0.5,
-  }).addTo(map);
+// Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
+//   }
+// }).addTo(map);
 
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location, {
-//         radius: city.population/200000,
-//         color: "orange",
-//     })
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>" + "Airport Name: " + feature.properties.name + "</h3>");
+    }  
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -41,6 +52,9 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
+   
 
 
    
