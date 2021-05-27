73f12b3765ca8fd3461ec6e6ab57647554e5e39a
Colin Bartoe <colinbartoe@Colins-MacBook-Pro.local>
2021-05-27 19:03:38 -0400

    updating polygons

--- mapping_geojson_linestrings/static/js/logic.js
+++ mapping_geojson_linestrings/static/js/logic.js
@@ -3,14 +3,14 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -18,8 +18,8 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Light: light,
-  Dark: dark
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center and zoom level.
@@ -27,19 +27,19 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 2,
-  layers: [dark]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
+// let myStyle = {
+//   color: "#ffffa1",
+//   weight: 2
+// }
 
 
 // Then we add our 'graymap' tile layer to the map.
@@ -47,7 +47,8 @@ let myStyle = {
 
 // Accessing the airport GeoJSON URL
 // let airportData = "https://raw.githubusercontent.com/ghynox/mapping_earthquakes/main/majorAirports.json";
-let torontoData = "https://raw.githubusercontent.com/ghynox/mapping_earthquakes/main/torontoRoutes.json";
+// let torontoData = "https://raw.githubusercontent.com/ghynox/mapping_earthquakes/main/torontoRoutes.json";
+let tHoods = "https://raw.githubusercontent.com/ghynox/mapping_earthquakes/main/torontoNeighborhoods.json"
 // Grabbing our GeoJSON data.
 // d3.json(airportData).then(function(data) {
 //   console.log(data) 
@@ -63,17 +64,26 @@ let torontoData = "https://raw.githubusercontent.com/ghynox/mapping_earthquakes/
 // });
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(tHoods).then(function(data) {
   console.log(data);
 
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
+  color: "blue",
+  weight: 1,
+  onEachFeature: function (feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + "Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
+  }
+//  {
   // color: "yellow",
   // weight: 2,
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-      layer.bindPopup("<h2>" + "Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
-  }
-}).addTo(map);
+  // style: myStyle,
+  // onEachFeature: function(feature, layer) {
+  //   console.log(layer);
+  //     layer.bindPopup("<h2>" + "Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
+  // }
+})
+.addTo(map);
 });
+
